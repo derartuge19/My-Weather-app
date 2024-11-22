32d4e89224afe47d00eedf881a01c6a54e4af136
derartuge19 <derartuge@gmail.com>
2024-11-22 15:06:25 +0300

    changing the weather information

--- WeatherApp/src/index.js
+++ WeatherApp/src/index.js
@@ -1,10 +1,9 @@
 function search(event) {
   event.preventDefault();
 
-  let searchInputElement = document.querySelector("#input-city"); // Match the input field id
+  let searchInputElement = document.querySelector("#input-city");
   let cityElement = document.querySelector(".current-city");
   let city = searchInputElement.value;
-  cityElement.innerHTML = city;
 
   let apiKey = "24aa0o8f50fa92cft1421f2bf2463b43";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
@@ -17,10 +16,24 @@ function search(event) {
         ".current-temperature-value"
       );
       temperatureElement.innerHTML = Math.round(temperature);
+      cityElement.innerHTML = response.data.city;
 
       let temperatureUnitElement = document.querySelector(
         ".current-temperature-unit"
       );
+      let descriptionDisplay = document.querySelector("#description");
+      let description = response.data.condition.description;
+      descriptionDisplay.innerHTML = description;
+      let humidityDisplay = document.querySelector("#humidity");
+      let humidity = response.data.temperature.humidity;
+      humidityDisplay.innerHTML = `${humidity}%`;
+      let windDisplay = document.querySelector("#wind-speed");
+      let wind = response.data.wind.speed;
+      windDisplay.innerHTML = `${wind}Km/hr`;
+      let date = new Date(response.data.time * 1000);
+      let time = document.querySelector("#time");
+      time.innerHTML = formatDate(date);
+
       temperatureUnitElement.innerHTML = "°C";
     })
     .catch((error) => {
@@ -28,6 +41,24 @@ function search(event) {
     });
 }
 
-// Add event listener to the form
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}, `;
+}
+
 let formElement = document.querySelector("form");
 formElement.addEventListener("submit", search);
